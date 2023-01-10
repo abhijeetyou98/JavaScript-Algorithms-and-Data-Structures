e1ab09463edd565bc84c3335fdddbe600ebdacda
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-10 23:00:56 +0530

    Update

--- Variables.js
+++ Variables.js
@@ -5,3 +5,14 @@ var myName;
 var a;
 // Only change code below this line
 a=7;
+//4
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+b = 7;
+//a should be assigned to b with =.
+ b =  a;
