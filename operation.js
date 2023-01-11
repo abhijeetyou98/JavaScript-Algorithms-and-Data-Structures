3d7306bfb3870b11b0706e3b73d7ad29e1fa11c1
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-11 20:18:19 +0530

    Update

---
+++ operation.js
@@ -0,0 +1,13 @@
+//Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+//Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+//Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+
+//Increment a Number with JavaScript
+
+let myVar = 87;
+
+// Only change code below this line
+myVar = ++myVar;
