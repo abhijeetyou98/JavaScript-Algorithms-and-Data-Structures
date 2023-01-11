8036bdd6d56b76b528f4f68f40c743d40ad8444b
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-11 18:36:32 +0530

    New_Update JavaScript

--- Variables.js
+++ Variables.js
@@ -29,4 +29,21 @@ c="I am a";
 a = a + 1;
 b = b + 5;
 c = c + " String!";
-//
+//6
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+//7
+let catName = "Oliver";
+let catSound = "Meow!";
+//8
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
