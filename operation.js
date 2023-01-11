2e507081613edab0ba2cf36546037a2ba7b9f814
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-11 20:36:46 +0530

    Update

--- operation.js
+++ operation.js
@@ -7,7 +7,65 @@ const quotient = 66 / 33;
 
 //Increment a Number with JavaScript
 
-let myVar = 87;
+let Var = 87;
 
 // Only change code below this line
-myVar = ++myVar;
+Var = ++Var;
+
+//Decrement a Number with JavaScript
+let myVar = 11;
+
+// Only change code below this line
+myVar = --myVar ;
+//Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+
+// Only change code below this line
+var myDecimal = 5.7;
+//Multiply Two Decimals with JavaScript
+const products = 2.0 * 2.5;
+
+//Divide One Decimal by Another with JavaScript
+const quotients = 4.4 / 2.0; // Change this line
+
+//Finding a Remainder in JavaScript
+
+const remainder = 11%3;
+
+//Compound Assignment With Augmented Addition
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9 ;
+c += 7;
+//Compound Assignment With Augmented Subtraction
+let ab = 11;
+let bc = 9;
+let ca = 3;
+
+// Only change code below this line
+ab -=6;
+bc -=15;
+ca -=1;
+//Compound Assignment With Augmented Multiplication
+let abc = 5;
+let bca = 12;
+let cab = 4.6;
+
+// Only change code below this line
+abc *=  5;
+bca *= 3 ;
+cab *= 10;
+//Compound Assignment With Augmented Division
+let aa = 48;
+let bb = 108;
+let cc = 33;
+
+// Only change code below this line
+aa /=  12;
+bb /=  4;
+cc /=  11;
+
