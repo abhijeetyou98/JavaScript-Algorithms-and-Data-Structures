356770fa59facf8243fd8ec5f371245c4acd29df
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-13 23:04:22 +0530

    Update

--- string.js
+++ string.js
@@ -1,4 +1,30 @@
+//Escape Sequences in Strings
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+//Concatenating Strings with Plus Operator
 const Str = "This is the start. "+"This is the end."; // Change this line
+//Concatenating Strings with the Plus Equals Operator
 let Str1 = "This is the first sentence. ";
 Str1 += "This is the second sentence.";
+//Constructing Strings with Variables
+// Only change code below this line
+const myName = "Abhijeet";
+const myStr1 = "My name is"+myName+"and I am well!";
+
+//Appending Variables to Strings
+
+// Change code below this line
+const someAdjective = "awesome!";
+let myStr2 = "Learning to code is ";myStr += someAdjective;
+
+//Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//Use Bracket Notation to Find the First Character in a String
+
+
+
