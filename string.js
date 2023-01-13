3fa073a4688f4ea40e6a36e234a189ac86c1ae6f
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-13 22:39:50 +0530

    Update

--- string.js
+++ string.js
@@ -1,1 +1,4 @@
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+const Str = "This is the start. "+"This is the end."; // Change this line
+let Str1 = "This is the first sentence. ";
+Str1 += "This is the second sentence.";
