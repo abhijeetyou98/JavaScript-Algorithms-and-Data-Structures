143a33f0b122f2f078fc128b677cd46a6d60cf8e
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-15 01:06:54 +0530

    Update

--- list.js
+++ list.js
@@ -1,2 +1,10 @@
 //Shopping List
 const myList = [["i-pnone",1],["i-pnone13",13],["i-pnoneX",10],["Mac",23,"Kit"],["i-pnone14",14]];
+
+//Write Reusable JavaScript with Functions
+function reusableFunction (){
+    console.log("Hi World");
+  }
+  reusableFunction();
+  
+
