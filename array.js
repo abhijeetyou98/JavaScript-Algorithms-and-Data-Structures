076653854b425304e069ae2ca554f9e66f63ea30
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-15 00:32:28 +0530

    Update

--- array.js
+++ array.js
@@ -26,6 +26,16 @@ const myArray = [["John", 23], ["cat", 2]];
 // Only change code below this line
 myArray.push(["dog", 3]);
 
+//Manipulate Arrays With pop()
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+var removedFromMyArray = myArray.pop();
+
+
+
+
 
 
 
