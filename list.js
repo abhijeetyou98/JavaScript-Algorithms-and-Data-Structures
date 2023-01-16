97f76fbf5cc7be976d25ed23f1d8b8fc4c55776f
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-16 22:38:03 +0530

    Update

--- list.js
+++ list.js
@@ -7,4 +7,17 @@ function reusableFunction (){
   }
   reusableFunction();
   
+  //Passing Values to Functions with Arguments
+
+  function functionWithArgs (a,b){
+    console.log(a+b);
+    }
+    functionWithArgs(1,2);
+
+  //Return a Value from a Function with Return
+    function timesFive(num){
+      return num*5;
+    } 
+    const answer = timesFive(5);
+
 
