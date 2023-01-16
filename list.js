6bddd6672eb604655dc2f467dc58ec71ca7cc683
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-16 22:50:45 +0530

    Update

--- list.js
+++ list.js
@@ -19,5 +19,27 @@ function reusableFunction (){
       return num*5;
     } 
     const answer = timesFive(5);
+// Global Scope and Functions
+// Declare the myGlobal variable below this line
 
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+oopsGlobal=5;
+}
+let myGlobal;
+myGlobal=10;
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
 
