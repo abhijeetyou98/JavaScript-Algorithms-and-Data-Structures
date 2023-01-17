c5355a81cd762f2d0881afb62768bb0f92203de0
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-17 21:46:13 +0530

    Update

--- list.js
+++ list.js
@@ -43,3 +43,62 @@ function fun2() {
   console.log(output);
 }
 
+//Local Scope and Functions
+function myLocalScope() {
+  // Only change code below this line
+const myVar="Hi";
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+// Run and check the console
+
+G//lobal vs. Local Scope in Functions
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+
+//Understanding Undefined Value returned from a Function
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive(){
+  sum = sum + 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
+
+//Assignment with a Returned Value
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
+
+
+//Stand in Line
+
+
