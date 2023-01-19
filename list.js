1ad2f5f3eccd2a32d3369e4af95a71bf7b89423c
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-19 20:30:18 +0530

    Update

--- list.js
+++ list.js
@@ -288,6 +288,45 @@ function testLogicalOr(val) {
 
 testLogicalOr(15);
 
+//Introducing Else Statements
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else  {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+
+//Introducing Else If Statements
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+  else  if (val < 5) {
+    return "Smaller than 5";
+  }
+else {
+  return "Between 5 and 10";
+}
+  
+}
+
+testElseIf(7);
+
+//Logical Order in If Else Statements
+ 
+
 
 
 
