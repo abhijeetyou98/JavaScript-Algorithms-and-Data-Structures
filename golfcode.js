5a45bad45c499ab268707f049aec8674798666f2
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-19 21:16:02 +0530

    Update

---
+++ golfcode.js
@@ -0,0 +1,30 @@
+//Golf Code
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes == 1) {
+  return "Hole-in-one!";
+}
+else if (strokes <= par - 2) {
+  return "Eagle";
+}else if (strokes == par - 1) {
+  return "Birdie";
+}else if (strokes == par ) {
+  return "Par";
+}
+else if (strokes == par + 1) {
+  return "Bogey";
+}else if (strokes <= par + 2) {
+  return "Double Bogey";
+}else if (strokes >= par + 3) {
+  return "Go Home!";
+}
+
+  
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+//
---
+++ switch.js
@@ -0,0 +1,33 @@
+// Switch
+//Selecting from Many Options with Switch Statements
+
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+  switch (val){
+    case 1:
+    answer ="alpha";
+    break
+  }switch (val){
+    case 2:
+    answer ="beta";
+    break
+  }switch (val){
+    case 3:
+    answer ="gamma";
+    break
+  }
+  switch (val){
+    case 4:
+    answer ="delta";
+    break
+  }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  caseInSwitch(1);
+
+  //
