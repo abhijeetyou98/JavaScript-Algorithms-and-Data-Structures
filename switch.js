0fe1d12eba60d7f3241c0f978787266f308a9d12
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-23 09:48:36 +0530

    Update code

--- switch.js
+++ switch.js
@@ -143,6 +143,43 @@ return undefined;
 
 abTest(2,2);
 //Counting Cards
+let count = 0;
 
+function cc(card) {
+  // Only change code below this line
+
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  
+}
+  return count + " " + holdbet;
+
+
+
+  
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+//
 
 
