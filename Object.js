1024920234b2f68c418436dcb27dac1a6f1ee146
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-24 18:51:11 +0530

    Updated code

--- Object.js
+++ Object.js
@@ -80,6 +80,24 @@ const myDog = {
   // Only change code below this line
   delete myDog.tails;
   //Using Objects for Lookups
+  function phoneticLookup(val) {
+    var result = "";
+    var lookup = {
+      "alpha": "Adams",
+      "bravo": "Boston",
+      "charlie": "Chicago",
+      "delta": "Denver",
+      "echo": "Easy",
+      "foxtrot": "Frank"
+    };
+  // After converting our case statements into object properties you can make use of the variable `result` to let the function return the correct value.
+  
+  
+    result = lookup[val];
+    // Only change code above this line
+    return result;
+  }
+  //
 
 
   
