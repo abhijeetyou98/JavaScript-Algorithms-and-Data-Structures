ccd34c34d6cbd33aa7dd3b60b36d7aeed2fe5955
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-01-26 17:41:25 +0530

    Updated code

--- Object.js
+++ Object.js
@@ -99,6 +99,27 @@ const myDog = {
   }
   //Testing Objects for Properties
 
+  //Iterate Odd Numbers With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 1; i < 10; i +=2 ){
+  myArray.push(i);
+}
+console.log(myArray);
+
+//Count Backwards With a For Loop
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -=2){
+  myArray.push(i);
+}
+console.log(myArray);
+
+//
 
 
   
