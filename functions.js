5bcc739186e30445619d546bdaac9ade2bf62489
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-05 23:51:07 +0530

    New Function Code

---
+++ functions.js
@@ -0,0 +1,13 @@
+//Compare Scopes of the var and let Keywords
+function checkScope() {
+    "use strict";
+    let i = "function scope";
+    if (true) {
+      let i = "block scope";
+      console.log("Block scope i is: ", i);
+    }
+    console.log("Function scope i is: ", i);
+    return i;
+  }
+
+  //
