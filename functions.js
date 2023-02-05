9c98183c05b054655ce3734a4c01650c7bd2188a
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-06 00:13:30 +0530

    new code

--- functions.js
+++ functions.js
@@ -10,4 +10,19 @@ function checkScope() {
     return i;
   }
 
-  //
+  //Mutate an Array Declared with const
+
+  const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+"use strict";
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+
+//
