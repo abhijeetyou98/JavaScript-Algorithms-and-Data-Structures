7213b2a62326f6af99e1c73ffa60393e1322b36f
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-07 09:57:49 +0530

    new_code

--- functions.js
+++ functions.js
@@ -43,5 +43,20 @@ Object.freeze(MATH_CONSTANTS)
 }
 const PI = freezeObj();
 
-//
+//Use Arrow Functions to Write Concise Anonymous Functions
+const magic = () => new Date();
+
+
+//Write Arrow Functions with Parameters
+const myConcat = (arr1, arr2)=> arr1.concat(arr2);
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+//Set Default Parameters for Your Functions
+// Only change code below this line
+const increment = (number, value=1) => number + value;
+// Only change code above this line
+
+//Use the Rest Parameter with Function Parameters
+
+
 
