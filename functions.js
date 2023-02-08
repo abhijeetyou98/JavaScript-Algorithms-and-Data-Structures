4061f97abf7cef075a79d36f14079fca43346538
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-08 20:32:34 +0530

    New_Code

--- functions.js
+++ functions.js
@@ -70,4 +70,20 @@ arr2 = [...arr1];  // Change this line
 
 console.log(arr2);
 
+//Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+
+const { today, tomorrow }= HIGH_TEMPERATURES;
+
+
+// Only change code above this line
+
+//
+
 
