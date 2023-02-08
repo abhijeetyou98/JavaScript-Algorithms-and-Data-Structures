162e690cd2440c10dd2099c38c71a41a7f5d24d6
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-08 20:38:07 +0530

    New_Code

---
+++ Objects.js
@@ -0,0 +1,27 @@
+//Use Destructuring Assignment to Extract Values from Objects
+const HIGH_TEMPERATURES = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+  
+  const { today, tomorrow }= HIGH_TEMPERATURES;
+  
+  
+  // Only change code above this line
+  
+  
+  //Use Destructuring Assignment to Assign Variables from Objects
+  const HIGH_TEMPERATURES1 = {
+    yesterday: 75,
+    today: 77,
+    tomorrow: 80
+  };
+  
+  // Only change code below this line
+    
+  const {today:highToday,tomorrow:highTomorrow} = HIGH_TEMPERATURES; 
+  
+  // Only change code above this line
