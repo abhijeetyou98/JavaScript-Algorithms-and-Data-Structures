594d533da8d743ddc6be26d0b96ca3f13b1b1f83
Abhijeet Chauhan <abhijeetyou98@gmail.com>
2023-02-10 20:00:50 +0530

    New_Code

--- Objects.js
+++ Objects.js
@@ -43,4 +43,29 @@ const HIGH_TEMPERATURES = {
   
   const failuresList = makeList(result.failure);
 
-  //
+  //Write Concise Object Literal Declarations Using Object Property Shorthand
+
+  const createPerson = (name, age, gender) => 
+  // Only change code below this line
+ ({
+     name,
+     age,
+     gender
+  });
+  // Only change code above this line
+
+
+  //Write Concise Declarative Functions with ES6
+// Only change code below this line
+const bicycle = {
+  gear: 2,
+  setGear (newGear) {
+    this.gear = newGear;
+  }
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
+
+//Use class Syntax to Define a Constructor Function
+
